f00eaadb2d9df51ab7adaece0e2afea814268aca
Nour Rayan <n6rayan@gmail.com>
2023-05-18 18:22:25 +0000

    feat(FLOW-1): boostrap api

---
+++ src/functions/index.ts
@@ -0,0 +1,10 @@
+import type { APIGatewayEvent } from 'aws-lambda';
+
+export const helloWorld = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello, World!'
+    })
+  }
+}
