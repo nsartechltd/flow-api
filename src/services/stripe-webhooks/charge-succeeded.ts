574f7b521eeb5d756591c4cca00e43d6e245b663
Nour Rayan <n6rayan@gmail.com>
2024-01-20 21:12:16 +0000

    chore(FLOW-17): log event

--- src/services/stripe-webhooks/charge-succeeded.ts
+++ src/services/stripe-webhooks/charge-succeeded.ts
@@ -4,6 +4,8 @@ import { Event } from '@middy/http-json-body-parser';
 export const chargeSucceeded = async (
   event: Event
 ): Promise<APIGatewayProxyResult> => {
+  console.log('Event received: ', event);
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: '',
