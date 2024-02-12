7f7be6e5964a9d78825314f3a80f8c29edac658a
Nour Rayan <n6rayan@gmail.com>
2024-02-12 13:39:28 +0000

    feat(FLOW-22): more test coverage

--- tests/api/contact.integration.ts
+++ tests/api/contact.integration.ts
@@ -10,6 +10,29 @@ describe('Contact API', () => {
   });
 
   describe('POST /contacts', () => {
+    it('should throw a 400 if the payload is wrong', async () => {
+      const response = await request
+        .post('/contacts')
+        .set({ Authorization: `Bearer ${token}` })
+        .send({
+          name: 1234,
+        });
+
+      expect(response.statusCode).toEqual(400);
+      expect(response.body).toEqual({
+        issues: [
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            message: 'Name must be a string',
+            path: ['body', 'name'],
+            received: 'number',
+          },
+        ],
+        name: 'ZodError',
+      });
+    });
+
     it('should create a contact', async () => {
       const response = await request
         .post('/contacts')
